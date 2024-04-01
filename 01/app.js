265db2738c45280dd61a569934b6a8f7ed00f841
mikroTesting <nowakowski.m1995@gmail.com>
2024-04-01 12:48:50 +0200

    task1 done

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,13 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+function isMoreThanFive(number) {
+  if (number > 5) {
+    for (let i = 5; i <= number; i++) {
+      console.log(i);
+    }
+  }
+  console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
+}
+
+isMoreThanFive(randomNumber);
