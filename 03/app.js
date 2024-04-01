89dc80522b1caf41cd006621fc2668e70f3ef09c
mikroTesting <nowakowski.m1995@gmail.com>
2024-04-01 13:06:02 +0200

    tak3 done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+  randomNumber = Math.floor(Math.random() * x + 1);
+  iteration++;
+  console.log(randomNumber);
+}
+console.log("////////");
+console.log(iteration);
+console.log(randomNumber);
