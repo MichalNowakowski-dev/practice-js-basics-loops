05c38c3b3fa5e00eb1fb2c26201d81323f93e193
mikroTesting <nowakowski.m1995@gmail.com>
2024-04-01 12:56:10 +0200

    task2 done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
-const x = 5;
+const x = 6;
 let result = 0;
+let i = 1;
+
+for (i; i <= x; i++) {
+  result += i;
+}
+
+console.log(result);
+
+// while (i <= x) {
+//   result += i;
+//   i++;
+// }
+
+// console.log(result);
